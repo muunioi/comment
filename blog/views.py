015150df82ea019afa649119e1d84f35f8ac6510
muunioi <mhyoo7095@gmail.com>
2020-09-22 23:56:47 +0900

    미완성 숙제....ㅇ-<-<

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blog, Comment
 from .forms import BlogForm, CommentForm
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def home(request):
@@ -73,3 +75,33 @@ def comment_delete(request, pk):
 
     else:
         return render(request, 'blog/comment_delete.html', {'object':comment})
+
+def signup(request):
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                request.POST['username'],
+                password = request.POST['password1']
+            )
+            auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('home')
+    return render(request, 'blog/signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None: #동일한 name, password가 존재한다면(없지 않다면)
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'blog/login.html')
+    else:
+        return render(request, 'blog/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+    return render(request, 'blog/signup.html')
--- commentprj/settings.py
+++ commentprj/settings.py
@@ -33,12 +33,24 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+
+    'django.contrib.sites',
+
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+
+    #allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    #provider 구글 페이스북 카톡 깃허브 등 소셜로그인 제공 업체
+    'allauth.socialaccount.providers.naver',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,3 +132,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+AUTHENTICATION_BACKENDS = (
+    #Needed to login by username in Django admin, regardless of 'allauth'
+    'django.contrib.auth.backends.ModelBackend',
+
+    #'allauth' specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
